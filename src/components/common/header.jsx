7dd52fdcf94cfc4df8088cdf24fb48bdeeb0c1a2
Abdullah Danyal Saeed <abdullah.saeed@isolta.com>
2018-04-07 02:24:56 +0300

    add data

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import * as HeaderData from '../../data/header.js';
 
 const Header = () => {
+    const { navigationLinks } = HeaderData;
     return (
         <div id="header-wrapper">
             <div id="header" className="container">
@@ -8,8 +10,8 @@ const Header = () => {
                 <p>A responsive HTML5 site template. Manufactured by HTML5 UP.</p>
                 <nav id="nav">
                     <ul>
-                        <li><a className="icon fa-home" href="/"><span>Introduction</span></a></li>
-                        <li><a className="icon fa-home" href="/about"><span>About</span></a></li>
+                        <li><a className="icon fa-home" href="/"><span>{navigationLinks.home}</span></a></li>
+                        <li><a className="icon fa-home" href="/about"><span>{navigationLinks.about}</span></a></li>
                         <li><a className="icon fa-retweet" href="right-sidebar.html"><span>Right Sidebar</span></a></li>
                         <li><a className="icon fa-sitemap" href="no-sidebar.html"><span>No Sidebar</span></a></li>
                     </ul>
---
+++ src/data/header.js
@@ -0,0 +1,4 @@
+export const navigationLinks = {
+    home: 'Introduction',
+    about: 'About'
+}
