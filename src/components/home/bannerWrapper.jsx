aa8fe7bde9dc8eeee34a7fa4077bcfe80145d756
Abdullah Danyal Saeed <abdullah.saeed@isolta.com>
2018-04-07 10:18:52 +0300

    add banner wrapper and main wrapper components

---
+++ src/components/home/bannerWrapper.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const BannerWrapper = () => {
+    return (
+        <div id="banner-wrapper">
+            <div class="inner">
+                <section id="banner" class="container">
+                    <p>Use this space for <strong>profound thoughts</strong>.<br />
+                        Or an enormous ad. Whatever.</p>
+                </section>
+            </div>
+        </div>
+    )
+}
+
+export default BannerWrapper;
