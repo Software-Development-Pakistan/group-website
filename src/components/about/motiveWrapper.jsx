80c02f63e5f395d5ce4f7471c8be4531b53c50e1
daniyalusmani <daniyal.usmani@dreambroker.fi>
2018-04-07 19:12:50 +0300

    ADD css fixes

--- src/components/about/motiveWrapper.jsx
+++ src/components/about/motiveWrapper.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const MotiveWrapper = () => {
   return (
-    <div id="banner-wrapper">
-      <div className="inner">
-        <section id="banner" className="container">
+    <div id="banner-wrapper" className="motive-wrapper">
+      <div>
+        <section>
           <header>
-            <p>Motive</p>
+            <h2 className="motive-header">Motive</h2>
           </header>
           <p className="motive-text">Create awareness in common people about Programming Languages and conventional techniques</p>
         </section>
